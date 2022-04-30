4bde79c15171f08adfbdd8d6ba16cc577a0c183f
Isaac Floriano <55949467+isaacfloriano@users.noreply.github.com>
2022-04-30 12:06:44 -0300

    Update connet.py

--- connet.py
+++ connet.py
@@ -1,14 +1,29 @@
 import os
 import sys
-import random
 
-# função para trocar a rede
+# função para alternando a rede
 def wifi():
+    # abrir o txt e ver qual no nome dentro
+    with open("cache.txt", "r+") as f:
+        nome = f.read().split()
 
-	# Nome das redes conectadas
-	ssid = "SILVA|PUGNO"
+        # verificar se existe o nome
+        for SSID in nome:
 
-	# Conectar a rede aleatória
-	name_of_router = random.choice(ssid.split("|"))
-	os.system(f'''cmd /c "netsh wlan connect name={name_of_router}"''')
-	print(f"Conectado a rede {name_of_router}!")
+            # Condições para verificar a rede no cache
+            if "SILVA" in SSID:
+                # limpar chace
+                with open("cache.txt",'w') as f:
+                    # escrever no chace
+                    f.write("PUGNO")
+
+                    # altera rede
+                    os.system(f'''cmd /c "netsh wlan connect name={SSID}"''')
+                    print(f"Alternando para rede {SSID}!")
+
+            elif "PUGNO" in SSID:
+                with open("cache.txt",'w') as f:
+                    f.write("SILVA")
+
+                    os.system(f'''cmd /c "netsh wlan connect name={SSID}"''')
+                    print(f"Alternando para rede {SSID}!")
